d57a6eccc4ddcde41ab1ae887ce1a8cac7b1379b
Timothy Tomas <timothyjtee@gmail.com>
2022-03-04 20:26:37 -0800

    Added mouseover events for each grid element

--- template/js/main.js
+++ template/js/main.js
@@ -1,11 +1,18 @@
 //Make container 512px
 const container = document.querySelector('.container');
 
-
 //Create 256 square divs
 //Append squareDivs as child to .container
 for(let i = 0; i < 256; i++) {
   let squareDiv = document.createElement('div');
   squareDiv.className = 'squareDiv';
   container.appendChild(squareDiv);
+
+  // Give each div element a mouseover event w/grey background change
+  squareDiv.addEventListener('mouseover', () => {
+    squareDiv.style.backgroundColor = 'grey';
+ });
 }
+
+
+
