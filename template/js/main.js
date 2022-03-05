65c5470ad791ba1cc2db9fa2a2fac8b6015d71ff
Timothy Tomas <timothyjtee@gmail.com>
2022-03-04 16:18:20 -0800

    Setting container to 512px and individual square to 32px

--- template/js/main.js
+++ template/js/main.js
@@ -1,1 +1,12 @@
+//Make container 512px
+const container = document.querySelector('.container');
 
+
+//Creating individual square div 32px
+const squareDiv = document.createElement('div');
+squareDiv.style.width = '32px';
+squareDiv.style.height = '32px';
+squareDiv.style.border = '2px solid red';
+
+// append squareDiv as child to .container
+container.appendChild(squareDiv);
