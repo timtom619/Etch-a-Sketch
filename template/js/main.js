6fdb9d85e66d9aa4908302e5b11e77c99cb9399a
Timothy Tomas <timothyjtee@gmail.com>
2022-03-05 00:08:58 -0800

    Added functionality to remove original grid and create new user defined grid

--- template/js/main.js
+++ template/js/main.js
@@ -1,19 +1,26 @@
 //Make container 512px
 const container = document.querySelector('.container');
-
+// let arrayContainer;
+let list;
 //Create 256 square divs
 //Append squareDivs as child to .container
 
-for(let i = 0; i < 256; i++) {
-  let squareDiv = document.createElement('div');
-  squareDiv.className = 'squareDiv';
-  container.appendChild(squareDiv);
-
-  // Give each div element a mouseover event w/grey background change
-  squareDiv.addEventListener('mouseover', () => {
-    squareDiv.style.backgroundColor = 'grey';
- });
+function createGrid(gridSize) {
+  for(let i = 0; i < (gridSize * gridSize); i++) { // rethink this conditional 
+    let squareDiv = document.createElement('div');
+    squareDiv.className = 'squareDiv';
+    container.appendChild(squareDiv);
+  
+    // Give each div element a mouseover event w/grey background change
+    squareDiv.addEventListener('mouseover', () => {
+      squareDiv.style.backgroundColor = 'grey';
+   });
+  }
+  // creating a nodelist and storing in list to be used in setNewGridSize() 
+  list = document.querySelectorAll('.squareDiv');
 }
+
+createGrid(256);
 
 // Add a button to the top of the screen which will clear the current grid and send the user a popup asking for the number of squares per side for the new grid. Once entered, the new grid should be generated in the same total space as before (e.g. 960px wide) so that you’ve got a new sketch pad. Tip: Set the limit for the user input to a maximum of 100. A larger number of squares results in more computer resources being used, potentially causing delays, freezing, or crashing that we want to prevent.
 
@@ -26,10 +33,28 @@ let newGridDimensions = 0;
 // Add a button to the top of the screen which will clear the current grid
 const reset = document.querySelector('.reset').addEventListener('click', () => {
   let collection = container.children;
+
   for(let i = 0; i < collection.length; i++) {
     collection[i].style.backgroundColor = 'white';
   }
 
   //Send popup asking for the number of squares per side for new grid
   newGridDimensions = +prompt('Select a new grid size:');
+  while(newGridDimensions > 100) {
+    newGridDimensions = +prompt('Please select a grid size lower than 100.');
+  }
+  
+  setNewGridSize();
+
 });
+//Define a function that sets new grid size bases on newGridDimensions
+function setNewGridSize() {
+  //removes all child divs from parent .container
+  for (let i of list) {
+    i.remove();
+  }
+  
+  //Create new grid based on user input.
+  createGrid(newGridDimensions);
+}
+
